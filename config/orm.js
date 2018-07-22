00e6d616afefc21867d06cda9cb27bb1f04c2dbb
Mikala Smith <mikalasmith@Mikalas-MacBook-Air.local>
2018-07-21 22:00:27 -0400

    updates

--- config/orm.js
+++ config/orm.js
@@ -1,63 +1,40 @@
-var connection = require ("../config/connection.js");
+var connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
     var arr = [];
-  
+
     for (var i = 0; i < num; i++) {
-      arr.push("?");
+        arr.push("?");
     }
-  
+
     return arr.toString();
-  }
-  var orm = {
-    selectAll: function(tableInput, cb) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
+}
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    update: function (tableInput, condition, cb) {
+        connection.query('UPDATE ' + tableInput + ' SET devoured=true WHERE id=' + condition + ';', function (err, result) {
+            if (err) throw err;
+            cb(result);
+
+        })
     },
 
-    insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-    
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-    
-        console.log(queryString);
-    
-        connection.query(queryString, vals, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
-      },
-      // An example of objColVals would be {name: panther, sleepy: true}
-      updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-    
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-    
-        console.log(queryString);
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
-      },
-}
+    create: function(tableInput,val,cb){
+        connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ('"+val+"');", function(err,result){
+            if(err)throw err;
+            cb(result);
+
+        })
+    }
+};
+
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -7,16 +7,14 @@ var burger = {
         })
 
     },
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    update: function(id, cb) {
+        orm.update("burgers",id,cb) 
           cb(res);
-        });
+        
     },
-    update: function(objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function(res) {
-          cb(res);
-        });
+    create: function(name, cb) {
+        orm.create("burgers", name, cb); 
     }
-}
+};
 
 module.exports = burger;
